c251d9212fd3a2fb3cf1acd53089ca6d94eaa521
jsabook <73977548+jsabook@users.noreply.github.com>
2021-01-19 23:00:17 +0800

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,129 @@
+<template>
+  <div id="app">
+    <div class="big-wheel-box">
+      <big-wheel
+        width="3rem"
+        height="3rem"
+        ref="bigWheel"
+        :prizeList="prizeList"
+        @over="over">
+        <template v-slot:item="{ item }">
+          <div class="prize-name">{{item.txt}}</div>
+          <img class="prize-img" :src="item.img">
+        </template>
+      </big-wheel>
+
+      <!-- 开始按钮 -->
+      <img class="btn-go" @click="go" :src="require('@/assets/img/go.png')" />
+    </div>
+  </div>
+</template>
+
+<script>
+import BigWheel from '@/components/BigWheel'
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      prizeList: [
+        {
+          txt: '帅比视频点歌',
+          img: require('@/assets/img/dog.jpg')
+        },
+        {
+          txt: '发送素颜照',
+          img: require('@/assets/img/dog.jpg')
+        },
+        {
+          txt: '五块钱红包',
+          img: require('@/assets/img/dog-2.jpg')
+        },
+        {
+          txt: '真心话',
+          img: require('@/assets/img/dog.jpg')
+        },
+        {
+          txt: '大冒险',
+          img: require('@/assets/img/dog.jpg')
+        },
+      ],
+      valve: false, // 防止连续点击
+    }
+  },
+  methods: {
+    // 开始转动
+    go() {
+      if (this.valve) {
+        return
+      }
+      this.valve = true
+
+      // 模拟随机数，这里可以请求后台获取中将信息
+      const index = Math.floor(Math.random() * this.prizeList.length)
+      this.winningIndex = index
+      
+      // 转动转盘
+      this.$refs.bigWheel.rotate(index)
+    },
+    // 转盘转完事件
+    over(prizeInfo) {
+      alert(`恭喜获得${prizeInfo.txt}`)
+      this.valve = false
+    }
+  },
+  components: {
+    BigWheel
+  }
+}
+</script>
+
+<style lang="scss">
+#app {
+  position: relative;
+  overflow: hidden;
+  width: 100vw;
+  min-height: 100vh;
+  background-image: url('~@/assets/img/wheel_bg.jpg');
+  background-repeat: no-repeat;
+  background-position: center top;
+  background-size: 100%;
+  .big-wheel-box {
+    position: absolute;
+    top: 1.7rem;
+    left: 50%;
+    transform: translateX(-50%);
+    text-align: center;
+    font-size: 0;
+    background-image: url('~@/assets/img/disk_bg.png');
+    background-repeat: no-repeat;
+    background-position: center;
+    background-size: 100%;
+    padding: .22rem;
+    .prize-name {
+      position: absolute;
+      left: .13rem;
+      right: .13rem;
+      top: .2rem;
+      font-size: .14rem;
+      text-align: center;
+      color: #7D2A00;
+    }
+    .prize-img {
+      position: absolute;
+      top: .58rem;
+      left: 50%;
+      transform: translateX(-50%);
+      width: .3rem;
+      height: .3rem;
+    }
+    .btn-go {
+      position: absolute;
+      top: 1.19rem;
+      left: 50%;
+      transform: translateX(-50%);
+      width: .8rem;
+    }
+  }
+}
+</style>
